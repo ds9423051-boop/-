6e17728bd1d9780af86985fdd141241dc3ca1ae8
ds9423051-boop <ds9423051@stud.juntendo.ac.jp>
2026-05-12 03:04:06 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-```js id="h3zkw0"
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-module.exports = app;
-```
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('server started');
+});
