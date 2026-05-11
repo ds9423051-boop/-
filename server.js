018da91368363997d0b366f6ce659936f6ef393b
ds9423051-boop <ds9423051@stud.juntendo.ac.jp>
2026-05-12 02:50:21 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+```js id="h3zkw0"
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+module.exports = app;
+```
